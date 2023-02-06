cc2c6fd91d894815d8f77cf3ccaecafd389b02e2
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-06 22:58:36 +0300

    6

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,10 @@
     attr: 'sf.gif',
     text: 'page',
 		href: 'page.html',
+    num: 5,
+    num1: 1,
+		num2: 2,
+		num3: 3,
 	}
 }
   }
@@ -17,12 +21,11 @@
 		<h1> Vue exercises </h1>
 	</div>
   <br>
-  <br>
-  <br>
-  <br>
   <img :src="attr">
   <div> <p> Hello, i'm {{ name }} {{ surn }}</p>
-    <p>Check out my skills <a href="page.html">here</a></p>
+    <p>Check out my skills <a href="page.html">here</a></p> <br>
+    <p class="sc">Квадрат num равен {{num*num}}, А сумма num1,num2,num3 равна {{num1+num2+num3}}.</p>
+
   </div>
 </template>
 
@@ -32,12 +35,16 @@ header {
   line-height: 1.5;
 }
 
+.sc {
+  font-size: medium;
+ color: aqua;
+}
 
 p {
   line-height: 2.5;
   font-size: large;
   letter-spacing: 2px;
-  font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
   color: rgb(255, 0, 0);
 }
 
