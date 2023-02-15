25ac5d277ab6c81da84277ac0eba8f1488a4cafb
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-15 18:47:29 +0300

    22

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,20 @@
 export default {
   data() {
     return {
-      visibility_p1: false,
-      visibility_p2: false,
-      visibility_p3: false,
+      isAdmin: true,
     }
   },
   methods: {
-      toggle_p1: function() {
-        this.visibility_p1 = !this.visibility_p1;
-      },
-      toggle_p2: function() {
-        this.visibility_p2 = !this.visibility_p2;
-      },
-      toggle_p3: function() {
-        this.visibility_p3 = !this.visibility_p3;
-      },
+    toggle: function() {
+      this.isAdmin = !this.isAdmin;
+    }
   }
 }
 </script>
 <template>
-  <button class="button" @click="toggle_p1">{{ visibility_p1 ? 'hide' : 'show' }}</button><br>
-  <p v-if="visibility_p1">text p1</p>
-  <button class="button" @click="toggle_p2">{{ visibility_p2 ? 'hide' : 'show' }}</button><br>
-  <p v-if="visibility_p2">text p2</p>
-  <button class="button" @click="toggle_p3">{{ visibility_p3 ? 'hide' : 'show' }}</button><br>
-  <p v-if="visibility_p3">text p3</p>
-  
+  <button class="button" @click="toggle">{{ isAdmin ? "Выдать админку" : "Забрать админку" }}</button><br>
+  <p v-if="isAdmin">Пользователь сайта не являеется администратором</p>
+  <p v-else>Вы администратор!</p>
 </template>
 
 <style >
