0e4840fb8858c2fe2d34bb35adbfd15fc4f40314
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-22 22:25:02 +0300

    45

--- src/App.vue
+++ src/App.vue
@@ -2,60 +2,32 @@
 export default {
   data() {
     return {
-      num: 1,
-      num_square: "Задание 1",
-      numero_uno: 0,
-      numero_dos: 0,
-      sum_num: "Задание 2",
-      text_uno: "placeholder",
-      text_dos: "placeholder",
-      fullName: "",
-      userName: "",
-      userSurname: "",
-      userPatronymic: "",
+      value: "",
+      value_button: "",
+      value_array: "",
     };
   },
   methods: {
-    square: function () {
-      this.num_square = this.num ** 2;
-    },
-    sum: function () {
-      this.sum_num = this.numero_uno + this.numero_dos;
-    },
-    swap: function () {
-      const temp = this.text_uno;
-      this.text_uno = this.text_dos;
-      this.text_dos = temp;
-    },
-    split: function () {
-      this.userSurname = this.fullName.split(" ")[0];
-      this.userName = this.fullName.split(" ")[1];
-      this.userPatronymic = this.fullName.split(" ")[2];
+    split_value: function () {
+      this.value_array = this.value_button.split(" ");
     },
   },
 };
 </script>
 <template>
-<input type="num" v-model="num" />
-  <button @click="square">Квадрат num</button>
+ <valuearea v-model="value"></valuearea>
   &nbsp;
-  <p>{{ num_square }}</p>
+  <p>{{ value }}</p>
   &nbsp;
-  <input type="num" v-model="numero_uno" />
-  <input type="num" v-model="numero_dos" />
-  <button @click="sum">Сумма</button>
+  <valuearea v-model="value_button"></valuearea>
   &nbsp;
-  <p>{{ sum_num }}</p>
+  <button class="button" @click="split_value">Массив</button>
   &nbsp;
-  <input type="text" v-model="text_uno" />
-  <input type="text" v-model="text_dos" />
-  <button @click="swap">Замена</button>
-  &nbsp;
-  <input type="text" v-model="fullName" />
-  <button @click="split">Поделить ФИО</button>
-  <p>Фамилия = {{ userSurname }}</p>
-  <p>Имя = {{ userName }}</p>
-  <p>Отчество = {{ userPatronymic }}</p>
+  <ul v-for="elem in value_array">
+    <li :key="key">
+      {{ elem }}
+    </li>
+  </ul>
 </template>
 
 <style >
