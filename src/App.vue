2a0429b3a85210b148192974c00c8f04a998bcf1
Lir00f <nikolaenko.e.i.21@gmail.com>
2023-02-28 17:30:07 +0300

    57

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,13 @@
 <script>
 export default {
   data() {
-    return {
-      developers: [
-        {
-          id: 1,
-          name: "Alex",
-          salary: 100,
-          age: 30,
-          isEdit: false,
-        },
-        {
-          id: 2,
-          name: "Diana",
-          salary: 200,
-          age: 40,
-          isEdit: false,
-        },
-        {
-          id: 3,
-          name: "Polina",
-          salary: 300,
-          age: 50,
-          isEdit: false,
-        },
-      ],
-    };
+    return {};
   },
-  methods: {
-    removeItem: function (id) {
-      this.developers = this.developers.filter((developer) => {
-        return developer.id !== id;
-      });
-    },
-    edit: function (developer) {
-      developer.isEdit = true;
-    },
-    save: function (developer) {
-      developer.isEdit = false;
-    },
-  },
+  methods: {},
 };
 </script>
 <template>
-  <table>
-    <tr>
-      <th>ID</th>
-      <th>Имя</th>
-      <th>Зарплата</th>
-      <th>Возраст</th>
-    </tr>
-    <tr v-for="developer in developers">
-      <template v-if="!developer.isEdit">
-        <td>{{ developer.id }}</td>
-        <td>{{ developer.name }}</td>
-        <td>{{ developer.salary }}</td>
-        <td>{{ developer.age }}</td>
-        <td><button @click="removeItem(developer.id)">remove</button></td>
-        <td><button @click="edit(developer)">edit</button></td>
-      </template>
-      <template v-else>
-        <td><input v-model="developer.id" /></td>
-        <td><input v-model="developer.name" /></td>
-        <td><input v-model="developer.salary" /></td>
-        <td><input v-model="developer.age" /></td>
-        <td><button @click="save(developer)">save</button></td>
-      </template>
-    </tr>
-  </table>
+  
 </template>
 
 <style >
